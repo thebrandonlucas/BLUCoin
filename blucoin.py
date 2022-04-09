cbf552c7bd8f6fab0dedf4372d338c44c2ff42f2
Brandon Lucas <brandonlucas@Brandons-MacBook-Air.local>
2022-04-09 10:55:35 -0400

    Add proof-of-work

--- blucoin.py
+++ blucoin.py
@@ -18,16 +18,25 @@
 # 4 - data (Blocks & Transactions)
 # 5 - Application (Smart Contracts)
 
+# Mining and Consensus/Proof of work
+
+# Mining is the process of looking for a valid "proof" of a miner discovering
+# a number below the "target", which is a SHA256 number with a certain number of 0's prepended
+# interpreted in little endian
+
 from time import time
 from hashlib import sha256
 from ecdsa import SigningKey, SECP256k1
+import random
 
 
 class Blockchain:
     def __init__(self) -> None:
         self.chain = []
         self.pending_transactions = []
-        self.difficulty = 0x0
+        # The number of prepended zeroes to the 256 bit target number
+        self.difficulty = 5
+        self.block_reward = 50
 
     def __str__(self):
         result = f"Blucoin Blockchain:\n\tDifficulty: {self.difficulty}"
@@ -40,15 +49,23 @@ class Blockchain:
         return result
 
     def get_latest_block(self):
-        return self.chain[-1]
+        if len(self.chain):
+            return self.chain[-1]
+        # Genesis block?
+        return Block()
 
-    def add_block(self, winning_nonce=0x0):
-        previous_hash = self.get_latest_block().hash()
-        new_block = Block(
-            previous_hash, time(), self.pending_transactions, winning_nonce
-        )
-        self.chain.append(new_block)
-        self.pending_transactions = []
+    def add_block(self, block, node):
+        # Check the block hash for proof of work
+        if block.hash()[: self.difficulty] <= "0" * self.difficulty:
+            self.chain.append(block)
+            self.pending_transactions = []
+            print(f"Block {block.hash()} added!")
+            print(f"Rewarding {self.block_reward} ")
+            node.reward = self.block_reward
+        else:
+            print(
+                f"Block {block.hash()} rejected. Does not satisfy difficulty {self.difficulty}"
+            )
 
 
 class Block:
@@ -57,11 +74,12 @@ class Block:
         previous_hash=None,
         timestamp=time(),
         transactions=[],
-        winning_nonce=0x0,
+        nonce=0,
     ) -> None:
         self.previous_hash = previous_hash
         self.timestamp = timestamp
         self.transactions = transactions
+        self.nonce = nonce
 
     def __str__(self) -> str:
         result = "Block:\n"
@@ -70,64 +88,173 @@ class Block:
         return result
 
     def hash(self):
-        block_data = [self.previous_hash, self.timestamp, self.pending_transactions]
-        return sha256(bytes(block_data)).hexdigest()
+        block_data = str(
+            [self.previous_hash, self.timestamp, self.transactions, self.nonce]
+        ).encode()
+        return sha256(block_data).hexdigest()
+
+
+class Node:
+    def __init__(self, blockchain) -> None:
+        self.blockchain = blockchain
+        self.reward = 0
+
+    def mine(self, block) -> int:
+        hash = block.hash()
+        # A valid hash is found when the hash is smaller than the difficulty number
+        while hash[: self.blockchain.difficulty] > "0" * self.blockchain.difficulty:
+            nonce = random.randint(1, 9999999999999)
+            block.nonce = nonce
+            hash = block.hash()
+        return nonce
 
 
 class Transaction:
-    def __init__(self, sender, recipient, amount, signature=None) -> None:
+    def __init__(
+        self,
+        sender,
+        recipient,
+        amount,
+        signature=None,
+    ) -> None:
         self.sender = sender
         self.recipient = recipient
         self.amount = amount
         self.signature = signature
 
     def __str__(self) -> str:
-        return f"TX: ({self.amount} Blu FROM sender: {self.sender.to_der().hex()} -> recipient: {self.recipient.to_der().hex()}), SIGNATURE: {self.signature}"
+        return f"TX: ({self.amount} Blu FROM sender: {self.sender.to_der().hex()} -> recipient: {self.recipient.to_der().hex()})"
 
 
 class Wallet:
-    def __init__(self, privkey=None) -> None:
-        if not privkey:
-            self.privkey = SigningKey.generate(curve=SECP256k1)
-            self.pubkey = self.privkey.verifying_key
-        # TODO: what if user provides their own privkey?
+    def __init__(
+        self, blockchain=None, node=None, privkey=None, nickname=None, peer_nicknames={}
+    ) -> None:
+        self.blockchain = blockchain
+        self.node = node
+        self.privkey = privkey if privkey else SigningKey.generate(curve=SECP256k1)
+        self.pubkey = self.privkey.verifying_key
+        self.nickname = nickname
+        self.peer_nicknames = peer_nicknames
 
-    def send(self, recipient, amount, blockchain):
+    def send(self, recipient, amount):
         # To send, we need to:
         # 1 - Create a transaction
         # 2 - Sign the transaction
         # 3 - Add to pool of pending transactions
         tx = Transaction(self.pubkey, recipient, amount)
-        signature = self.sign(tx)
-        tx.signature = signature
-        blockchain.pending_transactions.append(tx)
+        tx.signature = self.sign(tx)
+        self.blockchain.pending_transactions.append(tx)
 
     def sign(self, transaction):
         # In this case, the "message" is the bytes of our transaction
-        signature = self.privkey.sign(str(transaction).encode())
-        return signature
+        return self.privkey.sign(str(transaction).encode())
 
     def verify(self, signature, transaction):
-        return self.pubkey.verify(signature, transaction)
+        return self.pubkey.verify(signature, str(transaction).encode())
 
-    def unverified_balance(self, blockchain):
+    # def get_reward(self):
+
+
+    def unverified_balance(self):
         # Unverified balance are the unverified transactions in pending_transactions
         unverified_balance = 0
-        for tx in blockchain.pending_transactions:
+        for tx in self.blockchain.pending_transactions:
             if tx.recipient == self.pubkey:
                 unverified_balance += tx.amount
         return unverified_balance
 
-    def verified_balance(self, blockchain):
+    def verified_balance(self):
         # These are transactions that have been included in the blockchain
         verified_balance = 0
-        for block in blockchain.chain:
+        for block in self.blockchain.chain:
             for tx in block.transactions:
                 if tx.recipient == self.pubkey:
                     verified_balance += tx.amount
         return verified_balance
 
-    def balance(self, blockchain):
+    def balance(self):
         # loop thru blockchain and add up tx's sent to account
         # (assuming no coinbase tx's)
-        return self.verified_balance(blockchain) + self.unverified_balance(blockchain)
+        return self.verified_balance() + self.unverified_balance()
+
+    # TODO: Get txns and associate them with nicknames for ease of use!
+    def set_nickname(self, nickname):
+        self.nickname = nickname
+
+    def add_peer_nickname(self, verifying_key, peer_nickname):
+        # Change ECDSA library type "VerifyingKey" to DER string
+        peer_pubkey = self.der(verifying_key)
+        self.peer_nicknames[peer_pubkey] = peer_nickname
+
+    # FIXME: no longer using der format, rename
+    def der(self, pubkey):
+        """
+        Convert pubkey to hex string in der format
+        """
+        return pubkey.to_string().hex()
+
+    def get_mempool_txns(self):
+        """
+        Show all transactions between for user's wallet in the mempool waiting to be confirmed
+        """
+        txns = []
+        for tx in self.blockchain.pending_transactions:
+            if tx.recipient == self.pubkey or tx.sender == self.pubkey:
+                txns.append(tx)
+        return txns
+
+    def get_confirmed_txns(self):
+        """
+        Get Txns for user's wallet that have been confirmed and are on the blockchain
+        """
+        txns = []
+        for block in self.blockchain.chain:
+            for tx in block.transactions:
+                if tx.recipient == self.pubkey or tx.sender == self.pubkey:
+                    txns.append(tx)
+        return txns
+
+    def get_all_txns(self):
+        """
+        Get all sent and received Txns for the current wallet
+        """
+        return self.get_mempool_txns() + self.get_confirmed_txns()
+
+    def show_txns(self, txns):
+        """Display txns, mapping peer_nicknames when and user nickname where applicable
+        Sample txns output:
+            - Bob -> Alice: (10 Blu)
+            - Alice -> Chris: (1 Blu)
+            - Chris -> Alice: (3 Blu)
+        """
+        txns_string = ""
+        for tx in txns:
+            user_pubkey_hex = self.der(self.pubkey)
+            sender_hex = self.der(tx.sender)
+            recipient_hex = self.der(tx.recipient)
+
+            tx_string = "\t- "
+            if tx.sender == self.pubkey:
+                tx_string += (
+                    f"{self.nickname}" if self.nickname else f"{user_pubkey_hex}"
+                )
+            elif self.der(tx.sender) in self.peer_nicknames:
+                tx_string += f"{self.peer_nicknames[sender_hex]}"
+            else:
+                tx_string += sender_hex
+
+            tx_string += " -> "
+
+            if tx.recipient == self.pubkey:
+                tx_string += (
+                    f"{self.nickname}" if self.nickname else f"{user_pubkey_hex}"
+                )
+            elif recipient_hex in self.peer_nicknames:
+                tx_string += f"{self.peer_nicknames[recipient_hex]}"
+            else:
+                tx_string += recipient_hex
+
+            txns_string += tx_string + f": ({tx.amount} BLU)" + "\n"
+
+        print(txns_string)
--- test_blucoin.py
+++ test_blucoin.py
@@ -1,14 +1,153 @@
 from blucoin import *
+import unittest
 
-blockchain = Blockchain()
-brandon_wallet = Wallet()
-laura_wallet = Wallet()
 
-brandon_wallet.send(laura_wallet.pubkey, 10, blockchain)
-print(laura_wallet.balance(blockchain))
+def generate_transactions():
+    blockchain = Blockchain()
+    brandon_wallet = Wallet(blockchain)
+    laura_wallet = Wallet(blockchain)
+    tom_wallet = Wallet(blockchain)
 
-# print(laura_wallet.verify(blockchain.pending_transactions[0].signature, str(blockchain.pending_transactions[0]).encode()))
-# print(blockchain.pending_transactions[0])
-# print(str(blockchain))
-for tx in blockchain.pending_transactions:
-    print(tx)
+    brandon_wallet.set_nickname("Brandon")
+    brandon_wallet.add_peer_nickname(laura_wallet.pubkey, "Laura")
+    brandon_wallet.add_peer_nickname(tom_wallet.pubkey, "Tom")
+
+    brandon_wallet.send(laura_wallet.pubkey, 123)
+    laura_wallet.send(brandon_wallet.pubkey, 33)
+    brandon_wallet.send(tom_wallet.pubkey, 12)
+    tom_wallet.send(brandon_wallet.pubkey, 24)
+
+    return blockchain
+
+
+class TestBlockchain(unittest.TestCase):
+    def test_init(self):
+        blockchain = Blockchain()
+        self.assertEqual(blockchain.chain, [])
+        self.assertEqual(blockchain.pending_transactions, [])
+
+    def test_add_block(self):
+        blockchain = generate_transactions()
+
+        block = Block(
+            previous_hash=blockchain.get_latest_block().hash(),
+            transactions=blockchain.pending_transactions,
+        )
+        node = Node(blockchain)
+        # miner_wallet = Wallet(blockchain=blockchain, node=node)
+        proof = node.mine(block)
+        blockchain.add_block(block, node)
+
+        self.assertEqual(len(blockchain.chain), 1)
+        self.assertEqual(blockchain.pending_transactions, [])
+        self.assertEqual(blockchain.chain[0].hash(), block.hash())
+        self.assertEqual(node.reward, 50)
+
+
+class TestBlock(unittest.TestCase):
+    # def test_hash(self):
+    #     blockchain = Blockchain()
+    #     blockchain.add_block()
+    #     blockchain.add_block()
+
+    #     previous_hash_last_block = blockchain.get_latest_block().previous_hash
+    #     hash_of_first_block = blockchain.chain[0].hash()
+    #     self.assertEqual(previous_hash_last_block, hash_of_first_block)
+    # def test_hash(self):
+    # block = Block()
+    pass
+
+
+class TestNode(unittest.TestCase):
+    def test_mine(self):
+        blockchain = Blockchain()
+        brandon_wallet = Wallet(blockchain)
+        laura_wallet = Wallet(blockchain)
+        tom_wallet = Wallet(blockchain)
+
+        brandon_wallet.set_nickname("Brandon")
+        brandon_wallet.add_peer_nickname(laura_wallet.pubkey, "Laura")
+        brandon_wallet.add_peer_nickname(tom_wallet.pubkey, "Tom")
+
+        brandon_wallet.send(laura_wallet.pubkey, 123)
+        laura_wallet.send(brandon_wallet.pubkey, 33)
+        brandon_wallet.send(tom_wallet.pubkey, 12)
+        tom_wallet.send(brandon_wallet.pubkey, 24)
+
+        block = Block(transactions=blockchain.pending_transactions)
+
+        node = Node(blockchain)
+        proof = node.mine(block)
+
+        # Test that the proof correctly hashes
+        block_data = str(
+            [block.previous_hash, block.timestamp, block.transactions, proof]
+        ).encode()
+        self.assertEqual(sha256(block_data).hexdigest(), block.hash())
+
+        # Test that the hash is below the difficulty
+        self.assertLessEqual(
+            block.hash()[: blockchain.difficulty], "0" * blockchain.difficulty
+        )
+
+
+class TestWallet(unittest.TestCase):
+    def test_send(self):
+        blockchain = Blockchain()
+        my_wallet = Wallet(blockchain)
+        peer_wallet = Wallet(blockchain)
+        my_wallet.send(peer_wallet.pubkey, 10)
+
+        self.assertEqual(peer_wallet.balance(), 10)
+
+    def test_verify(self):
+        blockchain = Blockchain()
+        my_wallet = Wallet(blockchain)
+        peer_wallet = Wallet(blockchain)
+        my_wallet.send(peer_wallet.pubkey, 10)
+        tx = blockchain.pending_transactions[0]
+
+        self.assertEqual(my_wallet.verify(tx.signature, tx), True)
+
+    def test_unverified_balance_empty(self):
+        blockchain = Blockchain()
+        my_wallet = Wallet(blockchain)
+        self.assertEqual(my_wallet.unverified_balance(), 0)
+
+    def test_unverified_balance_not_empty(self):
+        blockchain = Blockchain()
+        my_wallet = Wallet(blockchain)
+        peer_wallet = Wallet(blockchain)
+        my_wallet.send(peer_wallet.pubkey, 10)
+
+        self.assertEqual(peer_wallet.unverified_balance(), 10)
+
+    def test_add_peer_nickname(self):
+        my_wallet = Wallet()
+        peer_wallet = Wallet()
+        my_wallet.add_peer_nickname(peer_wallet.pubkey, "Laura")
+
+        peer_pubkey_der = peer_wallet.pubkey.to_string().hex()
+        self.assertEqual(my_wallet.peer_nicknames[peer_pubkey_der], "Laura")
+
+    def test_show_txns(self):
+        blockchain = Blockchain()
+        brandon_wallet = Wallet(blockchain)
+        laura_wallet = Wallet(blockchain)
+        tom_wallet = Wallet(blockchain)
+
+        brandon_wallet.set_nickname("Brandon")
+        brandon_wallet.add_peer_nickname(laura_wallet.pubkey, "Laura")
+        brandon_wallet.add_peer_nickname(tom_wallet.pubkey, "Tom")
+
+        brandon_wallet.send(laura_wallet.pubkey, 123)
+        laura_wallet.send(brandon_wallet.pubkey, 33)
+        brandon_wallet.send(tom_wallet.pubkey, 12)
+        tom_wallet.send(brandon_wallet.pubkey, 24)
+
+        txns = brandon_wallet.get_all_txns()
+        brandon_wallet.show_txns(txns)
+
+
+if __name__ == "__main__":
+    unittest.main()
